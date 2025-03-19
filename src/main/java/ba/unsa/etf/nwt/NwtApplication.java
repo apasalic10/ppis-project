a248919ae2fb14bf454bfa9cb8f6db0c9a870eec
ihasicic1 <ihasicic1@etf.unsa.ba>
2025-03-19 11:04:55 +0100

    add User Microservice

---
+++ src/main/java/ba/unsa/etf/nwt/NwtApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwtApplication.class, args);
+	}
+
+}
